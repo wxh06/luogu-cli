b88bec3a62f74e6f87b069671c46d86313eef13e
汪心禾 <wangxinhe06@gmail.com>
2023-10-26 15:03:49 +0800

    cobra

---
+++ cmd/user.go
@@ -0,0 +1,67 @@
+/*
+Copyright © 2023 汪心禾
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/glamour"
+	"github.com/spf13/cobra"
+	"github.com/wxh06/luogu-cli/pkg/luogu"
+)
+
+var style string
+
+// userCmd represents the user command
+var userCmd = &cobra.Command{
+	Use:   "user",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		data, err := luogu.Request[luogu.UserData]("GET", "https://www.luogu.com.cn/user/"+args[0], nil)
+		if err != nil {
+			return
+		}
+
+		fmt.Println(data.CurrentData.User.Name)
+		introduction, err := glamour.Render(data.CurrentData.User.Introduction, style)
+		if err != nil {
+			return
+		}
+		fmt.Println(introduction)
+		return
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(userCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// userCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// userCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	userCmd.Flags().StringVarP(&style, "style", "s", "notty", "Markdown style")
+}
